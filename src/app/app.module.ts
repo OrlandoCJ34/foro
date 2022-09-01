bcd809fd34a6fd50fc3da6bb45ec6101ffbd840c
OrlandoCJ34 <OrlandoCJ34@gmail.com>
2022-09-01 14:05:07 -0500

    Acrualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PostComponent } from './post/post.component';
+import { MenuComponent } from './menu/menu.component';
 
+const routes: Routes = [
+  {path:'login', component: LoginComponent },
+  {path:'home', component: HomeComponent },
+  {path:'post', component: PostComponent },
+  {path:'**',component:LoginComponent}];
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    PostComponent
+    PostComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
